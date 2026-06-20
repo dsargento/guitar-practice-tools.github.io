d296e72e4c6e43d748c52bacb8f59c152c0f3736
dsargento <david.sargento10@gmail.com>
2026-06-20 16:17:31 +0200

    changing root note works

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-function Fretboard() {
-  const tuning = ["E", "A", "D", "G", "B", "E"];
-  const strings = tuning.map((value, key) => <String key={key} tuning={value}/>);
+'use client';
+import { useState } from "react";
+
+const notes = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
+
+function Fretboard({scale, tuning, root}) {
+  let localNotes = [...notes];
+
+  let index = localNotes.indexOf(root);
+  for (let i = 0; i < scale.length; i++) {
+    // problème, index + 1
+    if (index > notes.length) {
+      index = index - notes.length;
+    }
+    localNotes = localNotes.fill("", index + 1, index + scale[i]);
+    index = index + scale[i];
+  }
+
+  const strings = tuning.map((value, key) => <String key={key} tuning={value} localNotes={localNotes}/>);
 
   return (
     <div className="fretboard ">
@@ -11,13 +27,12 @@ function Fretboard() {
   );
 };
 
-function String({tuning}) {
-  let notes = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
+function String({tuning, localNotes}) {
 
   const start = notes.indexOf(tuning);
-  notes = notes.slice(start).concat(notes.slice(0, start));
+  localNotes = localNotes.slice(start).concat(localNotes.slice(0, start));
 
-  const displayNotes = notes.map((note, key) => <Note key={key} note={note}/>);
+  const displayNotes = localNotes.map((note, key) => <Note key={key} note={note}/>);
   return (
     <>
     {displayNotes}
@@ -26,53 +41,52 @@ function String({tuning}) {
 }
 
 function Note({note}) {
+  if (note !== "") {
   return (
-    <div className="note">
+    <div className="note flex flex-column flex-center">
     <p>
     {note}
     </p>
     </div>
   );
+  } else {
+  return (
+    <div className="note flex">
+    </div>
+  );
+  }
+}
+
+function RootNote({root, onRootChange}) {
+  const listNotes = notes.map(function(note, key) { 
+    if (root === note) {
+      return <li key={key} className="active"><a href="#">{note}</a></li>
+    } else {
+      return <li key={key}><a href="#" onClick={() => onRootChange(note)}>{note}</a></li>
+    }
+  });
+
+  return <ul className="rootNotes flex flex-row items-center items-stretch text-center sm:items-start sm:text-left">{listNotes}</ul>;
 }
 
 export default function Home() {
+  const [rootNote, setRootNote] = useState("B");
+  const scale = [2, 2, 1, 2, 2, 2, 1];
+  const tuning = ["E", "A", "D", "G", "B", "E"];
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <main className="">
+    <h1>Root note: {rootNote}</h1>
     <div className="flex flex-col items-center text-center sm:items-start sm:text-left">
 
-    <Fretboard />
-    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-    To get started, edit the page.tsx file.
-      </h1>
-    <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-    Looking for a starting point or more instructions? Head over to{" "}
-    <a
-    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    className="font-medium text-zinc-950 dark:text-zinc-50"
-    >
-    Templates
-    </a>{" "}
-    or the{" "}
-    <a
-    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    className="font-medium text-zinc-950 dark:text-zinc-50"
-    >
-    Learning
-    </a>{" "}
-    center.
-      </p>
+    <Fretboard scale={scale} tuning={tuning} root={rootNote}/>
+    <RootNote root={rootNote} onRootChange={setRootNote}/>
+    <select>
+    <option>E Standard</option>
+    </select>
     </div>
-    <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-    <a
-    className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    target="_blank"
-    rel="noopener noreferrer"
-    >
-    Documentation
-    </a>
-    </div>
+
     </main>
     </div>
   );
