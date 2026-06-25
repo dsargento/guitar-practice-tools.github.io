686471bc59b660a6f32cf0d3a871b725b0cf66d3
dsargento <david.sargento10@gmail.com>
2026-06-25 15:10:08 +0200

    github pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
-};
+  basePath: '/guitar-react-tools',};
 
 export default nextConfig;
